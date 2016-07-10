9caf4431fdb934a7a83482b862610b6ec57e2754
Sebastian Bolling <sebastian.bolling@gmail.com>
2016-07-10 19:35:43 +0200

    Added more reducercode

--- Reducers.js
+++ Reducers.js
@@ -5,6 +5,31 @@ const initialState = {
     todos: []
 }
 
+function todos(state = [], action) {
+    switch(action.type){
+            case ADD_TODO:
+            return [
+                    ...state.todos,
+                    {
+                        text: action.text,
+                        completed: false
+                    }
+                ]
+            })
+            case TOGGLE_TODO:
+            return state.map((todo, index) => {
+                if(index === action.index) {
+                    return Object.assign({}, todo, {
+                        completed: !todo.completed
+                    })
+                }
+            return todo
+            })
+            default:
+                return state
+    }
+}
+
 function todoApp(state = initialState, action) {
     switch(action.type){
         case SET_VISIBILITY_FILTER:
@@ -12,14 +37,9 @@ function todoApp(state = initialState, action) {
                 visibilityFilter: action.filter
             })
             case ADD_TODO:
+            case TOGGLE_TODO:
             return Object.assign({}, state, {
-                todos: [
-                    ...state.todos,
-                    {
-                        text: action.text,
-                        completed: false
-                    }
-                ]
+            todos: todos(state.todos, ation)
             })
             default:
             return state
