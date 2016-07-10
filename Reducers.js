e5bf5f52c00f4d81fbfd1761523003666bbfded6
Sebastian Bolling <sebastian.bolling@gmail.com>
2016-07-10 15:59:46 +0200

    added reduers

---
+++ Reducers.js
@@ -0,0 +1,27 @@
+import {VisibilityFilters} from './actions'
+
+const initialState = {
+    visibilityFilter: VisibilityFilters.SHOW_ALL,
+    todos: []
+}
+
+function todoApp(state = initialState, action) {
+    switch(action.type){
+        case SET_VISIBILITY_FILTER:
+            return Object.assign({}, state, {
+                visibilityFilter: action.filter
+            })
+            case ADD_TODO:
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos,
+                    {
+                        text: action.text,
+                        completed: false
+                    }
+                ]
+            })
+            default:
+            return state
+    }
+}
