d77150374a6bbfd888e93dec38a6f16302f63038
Sebastian Bolling <sebastian.bolling@gmail.com>
2016-07-24 22:24:46 +0200

    complteted todolist

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,4 +1,25 @@
 mport React, {PropTypes} from 'react'
 import Todo from './Todo'
 
-const TodoList = ()
+const TodoList = ({todos, onTodoClick}) => (
+<ul>
+    (todos.map(todo => 
+              <todo
+               key={todo.id}
+               {...todo}
+               onClick={() => onTodoClick(todo.id)}
+     />
+              ))
+</ul>
+)
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        completed: PropTypes.bool.isRequired,
+        text: PropTypes.string.isRequired
+    }).isRequired).isRequired,
+    onTodoClick: PropTypes.func.isRequired
+}
+
+export default TodoList
